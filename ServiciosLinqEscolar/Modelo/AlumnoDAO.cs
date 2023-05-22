822e8bcfa014eb5b995cb061d0a4e3507758fd81
BenjaminGarcia754 <linkdelira@gmail.com>
2023-05-22 17:07:34 -0600

    Se creo el DAO de Alumno que contiene las consultas a la base de datos mediante LINQ

---
+++ ServiciosLinqEscolar/Modelo/AlumnoDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosLinqEscolar.Modelo
+{
+    public static class AlumnoDAO
+    {
+        public static List<usuario> obtenerUsuarios()
+        {
+            DataClasesEscolarUVDataContext conexionBD= new DataClasesEscolarUVDataContext("escolaruvConnectionString");
+
+            IQueryable<usuario> usuariosBD = from usuarioQuery in conexionBD.usuario
+                                          select usuarioQuery;
+            return usuariosBD.ToList();
+        }
+
+        public static void iniciarSesion()
+        {
+
+        }
+    }
+}
